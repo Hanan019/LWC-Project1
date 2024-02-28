2f5e8e0b0a183a4cee671c19da063684cc219f58
Hanan Maqbol <hananmaqbol1@outlook.com>
2024-02-28 21:20:10 +0500

    here removed the track which is not used in last commit.

--- force-app/main/default/lwc/nestedObjectPrivateProperty/nestedObjectPrivateProperty.js
+++ force-app/main/default/lwc/nestedObjectPrivateProperty/nestedObjectPrivateProperty.js
@@ -1,4 +1,4 @@
-import { LightningElement, track } from "lwc";
+import { LightningElement } from "lwc";
 
 export default class NestedObjectPrivateProperty extends LightningElement {
   myObjectTest = { First: "Hanan", Last: "Maqbol" };
