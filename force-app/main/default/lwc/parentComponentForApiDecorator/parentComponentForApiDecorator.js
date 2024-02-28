6df1dead938fb21872dbe9cf034caf6ae99d175d
Hanan Maqbol <hananmaqbol1@outlook.com>
2024-02-28 22:48:02 +0500

    just edited the parent child compoent such that it does the value from  a property in Parent JS file is accessed in Child  Comp. then invoked the child component in the parent Compoent and displayed on the Ui

--- force-app/main/default/lwc/parentComponentForApiDecorator/parentComponentForApiDecorator.js
+++ force-app/main/default/lwc/parentComponentForApiDecorator/parentComponentForApiDecorator.js
@@ -1,3 +1,7 @@
-import { LightningElement } from 'lwc';
+// eslint-disable-next-line no-unused-vars
+import { LightningElement } from "lwc";
 
-export default class ParentComponentForApiDecorator extends LightningElement {}
+export default class ParentComponentForApiDecorator extends LightningElement {
+  //by defualt the propery will be private.
+  greeting = "We are going to be generous";
+}
