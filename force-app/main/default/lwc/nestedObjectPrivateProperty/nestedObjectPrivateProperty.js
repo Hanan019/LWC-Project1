46243ada33c4d7ed0199af6b3677c60a35a81556
Hanan Maqbol <hananmaqbol1@outlook.com>
2024-02-28 21:15:32 +0500

    //now when we are resetting the the object or array completely ,     //we can remove the we can remove the @track as well.

--- force-app/main/default/lwc/nestedObjectPrivateProperty/nestedObjectPrivateProperty.js
+++ force-app/main/default/lwc/nestedObjectPrivateProperty/nestedObjectPrivateProperty.js
@@ -1,20 +1,22 @@
 import { LightningElement, track } from "lwc";
 
 export default class NestedObjectPrivateProperty extends LightningElement {
-  @track myObjectTest = { First: "Hanan", Last: "Maqbol" };
+  myObjectTest = { First: "Hanan", Last: "Maqbol" };
   /*without @track decorator the function will work but will not render the value on the UI.*/
   handleClick() {
     this.myObjectTest.First = "Iqbal";
     this.myObjectTest.Last = "Khan";
   }
 
-  @track NewTask = ["Song", "Trap", "world"];
+  NewTask = ["Song", "Trap", "world"];
   addTaskHandler() {
     this.NewTask.push("Go On");
   }
   refreshHandler() {
     //resetting the object value name to default
     this.myObjectTest = { First: "Hanan", Last: "Maqbol" };
+    //now when we are resetting the the object or array completely ,
+    //we can remove the we can remove the @track as well.
     this.NewTask = [...this.NewTask, "Up Ahead"];
   }
 }
