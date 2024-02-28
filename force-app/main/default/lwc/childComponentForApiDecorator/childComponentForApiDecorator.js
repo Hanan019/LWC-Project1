9d1a67d9993af29c147d91603179a8878b0e133a
Hanan Maqbol <hananmaqbol1@outlook.com>
2024-02-28 23:28:45 +0500

    Invoking  the child component from parent and assinging the value form paren to child using @api

--- force-app/main/default/lwc/childComponentForApiDecorator/childComponentForApiDecorator.js
+++ force-app/main/default/lwc/childComponentForApiDecorator/childComponentForApiDecorator.js
@@ -2,4 +2,6 @@ import { LightningElement, api } from "lwc";
 
 export default class ChildComponentForApiDecorator extends LightningElement {
   @api display;
+  @api displayGreetings; //c/lightningComponent1
+  @api userdata;
 }
--- force-app/main/default/lwc/parentComponentForApiDecorator/parentComponentForApiDecorator.js
+++ force-app/main/default/lwc/parentComponentForApiDecorator/parentComponentForApiDecorator.js
@@ -4,4 +4,5 @@ import { LightningElement } from "lwc";
 export default class ParentComponentForApiDecorator extends LightningElement {
   //by defualt the propery will be private.
   greeting = "We are going to be generous";
+  myObject = { FName: "Ahmed ", LName: "Muneeb ", Profession: "Salesforce " };
 }
