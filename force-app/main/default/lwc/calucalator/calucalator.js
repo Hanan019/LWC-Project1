d3211dfbed21dab3da0f111cea551050fed15f28
Hanan Maqbol <hananmaqbol1@outlook.com>
2024-02-29 19:25:49 +0500

    created a calculator : on Day 20:

---
+++ force-app/main/default/lwc/calucalator/calucalator.js
@@ -0,0 +1,65 @@
+import { LightningElement } from "lwc";
+
+export default class Calculator extends LightningElement {
+  // Define properties to store two inputs from the user and the result.
+  numberone = "";
+  numbertwo = "";
+  result = 0;
+
+  // Handles input changes in the text fields.
+  changeHandler(event) {
+    let namee = event.target.name;
+    let valuee = event.target.value;
+    if (namee === "Number1") {
+      this.numberone = valuee;
+      console.log("New number 1", this.numberone);
+    } else if (namee === "Number2") {
+      this.numbertwo = valuee;
+      console.log("New number 2", this.numbertwo);
+    }
+  }
+
+  calculateInput(event) {
+    let labelElement = event.target.label;
+    if (labelElement === "Add") {
+      this.result = parseInt(this.numberone, 10) + parseInt(this.numbertwo, 10);
+    } else if (labelElement === "Subtract") {
+      this.result = parseInt(this.numberone, 10) - parseInt(this.numbertwo, 10);
+    } else if (labelElement === "Multiply") {
+      this.result = parseInt(this.numberone, 10) * parseInt(this.numbertwo, 10);
+    } else if (labelElement === "Divide") {
+      this.result = parseInt(this.numberone, 10) / parseInt(this.numbertwo, 10);
+    }
+
+    this.numberone = "";
+    this.numbertwo = "";
+  }
+  resetResultHandler() {
+    this.result = "";
+  }
+}
+//   // Performs addition.
+//   addHandler() {
+//     this.result = parseInt(this.numberone, 10) + parseInt(this.numbertwo, 10);
+//   }
+
+//   // Performs subtraction.
+//   subtractHandler() {
+//     this.result = this.numberone - this.numbertwo;
+//   }
+
+//   // Performs multiplication.
+//   multiplicationHandler() {
+//     this.result = this.numberone * this.numbertwo;
+//   }
+
+//   // Performs division.
+//   divisionHandler() {
+//     if (this.numbertwo !== 0) {
+//       this.result = this.numberone / this.numbertwo;
+//     } else {
+//       console.error("Division by zero error!");
+//     }
+//   }
+
+// Resets the result and input fields.
