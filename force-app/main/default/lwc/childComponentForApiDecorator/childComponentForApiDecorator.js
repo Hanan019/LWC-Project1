eaf7579ca1e31d9063b1255e4609b836a2a5f876
Hanan Maqbol <hananmaqbol1@outlook.com>
2024-02-29 15:38:55 +0500

    we have checked how the data is passed into the child component from parent then after after  passing it to child and using then child component is invoked back into the parent component.

--- force-app/main/default/lwc/childComponentForApiDecorator/childComponentForApiDecorator.js
+++ force-app/main/default/lwc/childComponentForApiDecorator/childComponentForApiDecorator.js
@@ -4,4 +4,5 @@ export default class ChildComponentForApiDecorator extends LightningElement {
   @api display;
   @api displayGreetings; //c/lightningComponent1
   @api userdata;
+  @api isUserAvailable = "false"; //necessary to declate to let LWC decide if its boolean.
 }
