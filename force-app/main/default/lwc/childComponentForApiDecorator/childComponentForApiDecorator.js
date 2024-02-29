1874d6e332cc4a3534f7f53f0bd09961ea5fb246
Hanan Maqbol <hananmaqbol1@outlook.com>
2024-02-29 15:51:34 +0500

    properties updated and child invoked in Parent.

--- force-app/main/default/lwc/childComponentForApiDecorator/childComponentForApiDecorator.js
+++ force-app/main/default/lwc/childComponentForApiDecorator/childComponentForApiDecorator.js
@@ -3,6 +3,6 @@ import { LightningElement, api } from "lwc";
 export default class ChildComponentForApiDecorator extends LightningElement {
   @api display;
   @api displayGreetings; //c/lightningComponent1
-  @api userdata;
+  @api userData;
   @api isUserAvailable = "false"; //necessary to declate to let LWC decide if its boolean.
 }
